a8cbb4e96e962985e272bc2e2b605e1a1a37ad60
Varvara Firsova <145842211+firsovaVI@users.noreply.github.com>
2025-03-25 19:40:29 +0300

    bandit.py

--- bandit.py
+++ bandit.py
@@ -1,17 +1,12 @@
 from __future__ import division
-
 import time
 import numpy as np
 
-
 class Bandit(object):
-
     def generate_reward(self, i):
         raise NotImplementedError
 
-
 class BernoulliBandit(Bandit):
-
     def __init__(self, n, probas=None):
         assert probas is None or len(probas) == n
         self.n = n
@@ -20,12 +15,7 @@ class BernoulliBandit(Bandit):
             self.probas = [np.random.random() for _ in range(self.n)]
         else:
             self.probas = probas
-
         self.best_proba = max(self.probas)
 
     def generate_reward(self, i):
-        # The player selected the i-th machine.
-        if np.random.random() < self.probas[i]:
-            return 1
-        else:
-            return 0
+        return 1 if np.random.random() < self.probas[i] else 0
